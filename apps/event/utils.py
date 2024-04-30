45841568c64af824c2e5eab5f7e8289629d891c1
ok7uz <komronbekobloev@gmail.com>
2024-05-01 03:08:59 +0500

    heroku

--- apps/event/utils.py
+++ apps/event/utils.py
@@ -2,7 +2,7 @@ from infobip_channels.sms.channel import SMSChannel
 
 
 def send_sms(user, relatives, event):
-    user_name = f'{user.first_name} + {user.last_name}'
+    user_name = f'{user.first_name} {user.last_name}'
     response_url = 'https://answer-connect-us.netlify.app/?user_id={user_id}&event_id={event_id}'
     channel = SMSChannel.from_auth_params({
         "base_url": "vv84lr.api.infobip.com",
@@ -15,7 +15,7 @@ def send_sms(user, relatives, event):
                 "destinations": [{'to': relative.phone_number}],
                 "from": "InfobipSMS",
                 "text": f"Salom {relative.name}! {user_name} sizni {event.title}ga taklif qilyapti. "
-                         "Ovoz berish: " + response_url.format(user_id=relative.id, event_id=event.id)
+                         "\nOvoz berish: " + response_url.format(user_id=relative.id, event_id=event.id)
             }]
         })
         print(sms)
