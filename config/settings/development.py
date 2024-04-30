faaad954c44d39cfa7147dc063d63a62bd192e42
ok7uz <komronbekobloev@gmail.com>
2024-05-01 02:59:50 +0500

    heroku

--- config/settings/development.py
+++ config/settings/development.py
@@ -6,9 +6,13 @@ ALLOWED_HOSTS = ['*']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'dc73v84vipmvnc',
+       'USER': 'gxsgerbfgtmlit',
+       'PASSWORD': '75071f7382e62f839a12178d0019ee0828d93e26ce02de9681acccd1bdb1120f',
+       'HOST': 'ec2-34-252-152-193.eu-west-1.compute.amazonaws.com',
+       'PORT': 5432,
+   }
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
