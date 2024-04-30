7fd6a8e87406136351cb680a5f8647d7752130fe
ok7uz <komronbekobloev@gmail.com>
2024-05-01 02:23:35 +0500

    send sms

--- apps/event/utils.py
+++ apps/event/utils.py
@@ -1,15 +1,13 @@
 from infobip_channels.sms.channel import SMSChannel
 
 
-response_url = 'https://connect-us-vebster.neftlify.app/response/?user_id={user_id}&event_id={event_id}'
-channel = SMSChannel.from_auth_params({
-    "base_url": "vv84lr.api.infobip.com",
-    "api_key": "ec4510d13f4a61ebdcf3a25835560cb3-0ce36fc2-ff14-4df2-a23f-9b0485fdc95c"
-})
-
-
 def send_sms(user, relatives, event):
     user_name = f'{user.first_name} + {user.last_name}'
+    response_url = 'https://answer-connect-us.netlify.app/?user_id={user_id}&event_id={event_id}'
+    channel = SMSChannel.from_auth_params({
+        "base_url": "vv84lr.api.infobip.com",
+        "api_key": "ec4510d13f4a61ebdcf3a25835560cb3-0ce36fc2-ff14-4df2-a23f-9b0485fdc95c"
+    })
     
     for relative in relatives:
         sms = channel.send_sms_message({
@@ -17,8 +15,7 @@ def send_sms(user, relatives, event):
                 "destinations": [{'to': relative.phone_number}],
                 "from": "InfobipSMS",
                 "text": f"Salom {relative.name}! {user_name} sizni {event.title}ga taklif qilyapti. "
-                         "Quyidagi havola orqali o`z ovozingizni bering. "
-                         + response_url.format(user_id=relative.id, event_id=event.id)
+                         "Ovoz berish: " + response_url.format(user_id=relative.id, event_id=event.id)
             }]
         })
         print(sms)
